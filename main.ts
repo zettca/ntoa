eea89d3d8dd58e7e20c8ae4ac0265f56a5da7b45
Bruno <zettca@users.noreply.github.com>
2023-09-22 01:15:11 +0100

    chore: remove flags dep. parse as strings

--- main.ts
+++ main.ts
@@ -1,15 +1,11 @@
 #!/usr/bin/env -S deno run
 
-import { parse } from "https://deno.land/std@0.151.0/flags/mod.ts";
 import ntoa from "./mod.ts";
 
 function main() {
-  const args = parse(Deno.args);
-  const numbers = args._ as string[];
+  const numbers = Deno.args;
 
-  if (numbers.length < 1) {
-    throw new Error("Pass numbers as arguments");
-  }
+  if (numbers.length < 1) throw new Error("Pass numbers as arguments");
 
   numbers.forEach((number) => {
     if (isNaN(parseInt(number))) {
